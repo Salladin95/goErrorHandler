922007136ca79e40b893708e95ee03955e35012d
khalid <shkhalid95@mail.ru>
2024-02-01 12:36:05 +0300

    feat: add convertValidationErrors

--- lib.go
+++ lib.go
@@ -2,6 +2,9 @@ package goErrorHandler
 
 import (
 	"fmt"
+	"github.com/go-playground/validator/v10"
+	"log"
+	"strings"
 )
 
 func BindRequestToBodyFailure(err error) error {
@@ -30,5 +33,28 @@ func ValidationFailure(messages map[string]string) error {
 }
 
 func OperationFailure(operation string, err error) error {
-	return NewError(ErrInternalFailure, fmt.Errorf("failed to %s: %s", operation, err.Error()))
+	msg := fmt.Sprintf("failed to %s: %s", operation, err.Error())
+	log.Println(msg)
+	return NewError(ErrInternalFailure, fmt.Errorf(msg))
+}
+
+// ConvertValidationErrors converts validation errors to a more readable format.
+func ConvertValidationErrors(err error) map[string]string {
+	// Assert that the error is of type validator.ValidationErrors
+	validationErrors, ok := err.(validator.ValidationErrors)
+	if !ok {
+		// Log a fatal error if the type assertion fails
+		log.Fatal("Unexpected error type during validation")
+	}
+
+	// Convert validation errors to a map for easier handling
+	validationErrorMap := make(map[string]string)
+	for _, fieldError := range validationErrors {
+		// Convert field names to lowercase for consistency
+		fieldName := strings.ToLower(fieldError.Field())
+		// Build a validation error message using the field tag
+		validationErrorMap[fieldName] = fmt.Sprintf("Validation failed - %s", fieldError.Tag())
+	}
+
+	return validationErrorMap
 }
